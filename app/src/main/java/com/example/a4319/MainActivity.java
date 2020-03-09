f714d39a92fd44641addcec43b3d166002a6a75e
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-03-09 15:51:31 -0600

    App and button images
    I change app and button images. However, the functions that are supposed to randomly pick a number based on the level of difficulty don't work. The issue is anytime I click on the buttons. the app is closed.

--- app/src/main/java/com/example/a4319/MainActivity.java
+++ app/src/main/java/com/example/a4319/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.a4319;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.annotation.NonNull;
+
+import java.util.Random;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button easyBtn, mediumBtn, difficultBtn;
+        easyBtn = findViewById(R.id.easyBtn);
+        mediumBtn = findViewById(R.id.mediumBtn);
+        difficultBtn = findViewById(R.id.difficultBtn);
+        easyBtn.setOnClickListener(this);
+        mediumBtn.setOnClickListener(this);
+        difficultBtn.setOnClickListener(this);
+        final Random myRandom = new Random();
+        final TextView randomNumber = findViewById(R.id.randomNumber);
     }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.easyBtn{
+                // Generate a random number range [10, 99]
+                randomNumber.setText(String.valueOf(myRandom.nextInt(99 - 10) + 10 + 1));
+                break;
+            }
+            case R.id.mediumBtn{
+                // Generate a random number range [100, 999]
+                randomNumber.setText(String.valueOf(myRandom.nextInt(999 - 100) + 100 + 1));
+                break;
+            }
+            case R.id.difficultBtn{
+                // Generate a random number range [1000, 9999]
+                randomNumber.setText(String.valueOf(myRandom.nextInt(9999 - 1000) + 1000 + 1));
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    };
 }
