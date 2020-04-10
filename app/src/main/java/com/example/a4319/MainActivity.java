3452a26050bd1f46290aaa9e2dde2d3b68eb7d6f
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-04-10 16:23:38 -0600

    When clicking on buttons, the app jumps out

--- app/src/main/java/com/example/a4319/MainActivity.java
+++ app/src/main/java/com/example/a4319/MainActivity.java
@@ -8,21 +8,29 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    TextView textView;
+    Button easy, medium, hard;
+    Random myRandom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
-        Button easyBtn, mediumBtn, difficultBtn;
-        easyBtn = findViewById(R.id.easyBtn);
-        mediumBtn = findViewById(R.id.mediumBtn);
-        difficultBtn = findViewById(R.id.difficultBtn);
-        easyBtn.setOnClickListener(this);
-        mediumBtn.setOnClickListener(this);
-        difficultBtn.setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.randomNumber);
+        easy = (Button) findViewById(R.id.easyBtn);
+        medium = (Button) findViewById(R.id.mediumBtn);
+        hard = (Button) findViewById(R.id.difficultBtn);
+
+        easy.setOnClickListener(this);
+        medium.setOnClickListener(this);
+        hard.setOnClickListener(this);
         final Random myRandom = new Random();
         final TextView randomNumber = findViewById(R.id.randomNumber);
     }
@@ -30,23 +38,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v){
         switch (v.getId()){
-            case R.id.easyBtn{
+            case R.id.easyBtn:
                 // Generate a random number range [10, 99]
-                randomNumber.setText(String.valueOf(myRandom.nextInt(99 - 10) + 10 + 1));
+                textView.setText(String.valueOf(myRandom.nextInt(99 - 10) + 10 + 1));
                 break;
-            }
-            case R.id.mediumBtn{
+
+            case R.id.mediumBtn:
                 // Generate a random number range [100, 999]
-                randomNumber.setText(String.valueOf(myRandom.nextInt(999 - 100) + 100 + 1));
+                textView.setText(String.valueOf(myRandom.nextInt(999 - 100) + 100 + 1));
                 break;
-            }
-            case R.id.difficultBtn{
+
+            case R.id.difficultBtn:
                 // Generate a random number range [1000, 9999]
-                randomNumber.setText(String.valueOf(myRandom.nextInt(9999 - 1000) + 1000 + 1));
+                textView.setText(String.valueOf(myRandom.nextInt(9999 - 1000) + 1000 + 1));
                 break;
-            }
+
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
         }
-    };
+    }
 }
