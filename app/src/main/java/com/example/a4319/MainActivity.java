f3df763c9e04dd9dc9a3db5c878dd1209fd9f625
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-04-26 02:06:00 -0600

    Add draw function but the app still does not work

--- app/src/main/java/com/example/a4319/MainActivity.java
+++ app/src/main/java/com/example/a4319/MainActivity.java
@@ -7,11 +7,11 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,33 +24,29 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    TextView textView;
+    TextView textView, myRect;
     Button easy, medium, hard;
     final Random myRandom = new Random();
     List<Integer> data = new ArrayList<Integer>();
 
-    ImageView imageView;
-    Bitmap bitmap;
-    Canvas canvas;
-    Paint paint;
-    float downx = 0, downy = 0, upx = 0, upy = 0;
+    DrawingView dv ;
+    private Paint mPaint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-        imageView = (ImageView) this.findViewById(R.id.myRectangleView);
-        float dw = imageView.getWidth();
-        float dh = imageView.getHeight();
+        dv = new DrawingView(this);
+        setContentView(dv);
 
-        bitmap = Bitmap.createBitmap((int) dw, (int) dh,
-                Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bitmap);
-        paint = new Paint();
-        paint.setColor(Color.GREEN);
-        imageView.setImageBitmap(bitmap);
-
-        imageView.setOnTouchListener((View.OnTouchListener) this);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
 
         textView = (TextView) findViewById(R.id.randomNumber);
         easy = (Button) findViewById(R.id.easyBtn);
@@ -71,19 +67,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.easyBtn:
                 // Generate a random number range [1, 10]
                 myNumber = myRandom.nextInt(11 - 1) + 1;
-                textView.setText(String.valueOf(myNumber));
+                textView.setText(String.valueOf(myNumber) + " = ? * ?");
                 break;
 
             case R.id.mediumBtn:
                 // Generate a random number range [11, 50]
                 myNumber = myRandom.nextInt(51 - 11) + 11;
-                textView.setText(String.valueOf(myNumber));
+                textView.setText(String.valueOf(myNumber) + " = ? * ?");
                 break;
 
             case R.id.difficultBtn:
                 // Generate a random number range [51, 100]
                 myNumber = myRandom.nextInt(101 - 51) + 51;
-                textView.setText(String.valueOf(myNumber));
+                textView.setText(String.valueOf(myNumber) + " = ? * ?");
                 break;
 
             default:
@@ -98,25 +94,101 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    public void onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                downx = event.getX();
-                downy = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                break;
-            case MotionEvent.ACTION_UP:
-                upx = event.getX();
-                upy = event.getY();
-                canvas.drawLine(downx, downy, upx, upy, paint);
-                imageView.invalidate();
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                break;
-            default:
-                break;
+    public class DrawingView extends View {
+
+        public int width;
+        public  int height;
+        private Bitmap  mBitmap;
+        private Canvas  mCanvas;
+        private Path    mPath;
+        private Paint   mBitmapPaint;
+        Context context;
+        private Paint circlePaint;
+        private Path circlePath;
+
+        public DrawingView(Context c) {
+            super(c);
+            context=c;
+            mPath = new Path();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLUE);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(4f);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawPath( mPath,  mPaint);
+            canvas.drawPath( circlePath,  circlePaint);
+        }
+
+        private float mX, mY;
+        private static final float TOUCH_TOLERANCE = 4;
+
+        private void touch_start(float x, float y) {
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+        }
+
+        private void touch_move(float x, float y) {
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y - mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                mX = x;
+                mY = y;
+
+                circlePath.reset();
+                circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
+            }
+        }
+
+        private void touch_up() {
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mPaint);
+            // kill this so we don't double draw
+            mPath.reset();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
         }
     }
 }
